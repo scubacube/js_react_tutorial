a34da89987499690afa1376b691f0ef7d2c293c7
Lexa <you@example.com>
2020-03-17 18:58:29 +0100

    Невозможно изменить прогресс для данного элемента

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('asdasd');
+let money  = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget : money ,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+
+let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце?");
+let firstAnswer = prompt("Во сколько обойдется?");
+appData.expenses.firstQuestion = firstAnswer;
+appData.expenses.firstQuestion = firstAnswer;
+
+let oneDayBudget = appData.budget / 30;
+
+// alert(oneDayBudget);
+// alert(appData.expenses.firstQuestion);
+// console.log(appData);
