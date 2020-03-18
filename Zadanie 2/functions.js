5fe3cf41e8c2694314191e6a1a50d94d5e2c406a
Lexa <you@example.com>
2020-03-18 11:58:57 +0100

    zadanie 3

---
+++ Zadanie 2/functions.js
@@ -0,0 +1,17 @@
+
+// function calc(a, b) {
+    
+//     return (a * b);
+// }
+
+// function dev() {
+//     let num = 90;
+//     return num;
+// }
+// let anotherNum = dev();
+// console.log(calc(6,6));
+
+// console.log(anotherNum);
+
+let calc1 = (a,b) => a + b;
+console.log(calc1(5,6));
