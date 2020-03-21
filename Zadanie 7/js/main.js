ccf1f8a7dbd5ae320a56df57411bbe5ceb8e9501
Lexa <you@example.com>
2020-03-21 17:02:40 +0100

    Zadanie 7

---
+++ Zadanie 7/js/main.js
@@ -0,0 +1,24 @@
+let start = document.getElementById('start');
+
+let budget_value = document.querySelector('.budget-value');
+let level_value = document.querySelector('.level-value');
+let expenses_value = document.querySelector('.expenses-valuee');
+let optionalexpenses_value = document.querySelector('.optionalexpenses-value');
+let monthsavings_value = document.querySelector('.monthsavings-value');
+let yearsavings_value = document.querySelector('.yearsavings-value');
+
+let expenses_item = document.querySelectorAll('.expenses-item');
+
+let count_budget_btn = document.querySelector('count-budget-btn');
+let expenses_item_btn = document.querySelector('expenses-item-btn');
+let optionalexpenses_btn = document.querySelector('optionalexpenses-btn');
+
+let optionalexpenses_item = document.querySelectorAll('.optionalexpenses-item');
+
+let income = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let choose_sum = document.querySelector('.choose-sum');
+let choose_percent = document.querySelector('.choose-percent');
+let year_value = document.querySelector('.year-value');
+let month_value = document.querySelector('.month-value');
+let day_value = document.querySelector('.day-value');
